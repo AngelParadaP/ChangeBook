4bb7861b0a93f29ea1455c9e39deef8c0790a1b0
AngelParadaP <paradapangel@gmail.com>
2026-02-22 22:48:55 -0600

    feat: Initialize Tailwind CSS configuration and remove `@tailwindcss/typography` dependency.

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+    content: [
+        "./app/**/*.{ts,tsx}",
+        "./components/**/*.{ts,tsx}",
+    ],
+    plugins: [require("@tailwindcss/typography")],
+};
+
+export default config;
