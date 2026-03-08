ab9ea23a6515ecdc32fb94226b584cb53da56047
Cesar Balam Espinosa Nuñez <cesarbalam.2004@gmail.com>
2026-03-07 23:05:34 -0600

    added vector tables for the recomendation

--- drizzle/0011_open_zarek.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE "community_vectors" (
-	"community_id" uuid PRIMARY KEY NOT NULL,
-	"embedding" vector(50) NOT NULL
-);
---> statement-breakpoint
-ALTER TABLE "community_vectors" ADD CONSTRAINT "community_vectors_community_id_communities_id_fk" FOREIGN KEY ("community_id") REFERENCES "public"."communities"("id") ON DELETE cascade ON UPDATE no action;
--- drizzle/0013_fluffy_gideon.sql
+++ drizzle/0013_fluffy_gideon.sql
@@ -3,10 +3,16 @@ CREATE TABLE "book_vectors" (
 	"embedding" vector(50) NOT NULL
 );
 --> statement-breakpoint
+CREATE TABLE "community_vectors" (
+	"community_id" uuid PRIMARY KEY NOT NULL,
+	"embedding" vector(50) NOT NULL
+);
+--> statement-breakpoint
 CREATE TABLE "user_vectors" (
 	"user_id" uuid PRIMARY KEY NOT NULL,
 	"embedding" vector(50) NOT NULL
 );
 --> statement-breakpoint
 ALTER TABLE "book_vectors" ADD CONSTRAINT "book_vectors_book_id_books_id_fk" FOREIGN KEY ("book_id") REFERENCES "public"."books"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "community_vectors" ADD CONSTRAINT "community_vectors_community_id_communities_id_fk" FOREIGN KEY ("community_id") REFERENCES "public"."communities"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "user_vectors" ADD CONSTRAINT "user_vectors_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
